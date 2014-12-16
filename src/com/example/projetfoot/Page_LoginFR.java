5fb3e56a3c0876c9f70e1aaaf22daacda4524032
alexandre <al.collette25@gmail.com>
2014-12-16 10:11:45 +0100

    Test 2

--- src/com/example/projetfoot/Page_LoginFR.java
+++ src/com/example/projetfoot/Page_LoginFR.java
@@ -13,7 +13,7 @@ public class Page_LoginFR extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page__login_fr);
     }
-// Test Alex
+// Test Alex 2
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
